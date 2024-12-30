5a58e019d1217118a0af24d6ad7990f1498b6f57
sinvalfelisberto <sinvalfelisberto@gmail.com>
2024-12-30 10:20:53 -0300

    corrigindo problemas com gitignore

---
+++ src/main/java/com/felisberto/agregadorinvestimentos/token/Token.java
@@ -0,0 +1,13 @@
+package com.felisberto.agregadorinvestimentos.token;
+
+public class Token {
+    private String token;
+
+    public Token() {
+        token = "dSeoDKisPhduHmVawmAyhH";
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
